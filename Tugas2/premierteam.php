ca036d879df44ce41963be853ea0cba66e851d82
Kaceee17 <146193981+Kaceee17@users.noreply.github.com>
2024-03-25 14:20:00 +0700

    update

--- Tugas2/premierteam.php
+++ Tugas2/premierteam.php
@@ -1,3 +1,10 @@
+<?php
+$sumber = 'https://apiv3.apifootball.com?action=get_teams&APIkey=7119d97d02940f99ce4271e9189d2df4bb98f44487a0a5f0785161b42f5532d8&league_id=152';
+$data = file_get_contents($sumber);
+$konten = json_decode($data, true);
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -38,13 +45,17 @@
     <div class="d-flex justify-content-center">
       <div class="col-md-10 card-container">
         <div class="row justify-content-center">
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Team</h5>
-              <p class="card-text">Team disetiap negara-negara di spanyol </p>
-              <a href="premierteam.php" class="btn btn-primary">Lihat lebih lanjut</a>
-            </div>
+          <?php foreach ($konten as $team) {
+          ?>
+          <div class="col-md-4 mb-3" style="width: 15rem;">
+            <div class="card shadow-sm p-3 mb-5 bg-white rounded mx-auto " >
+              <img src=" <?php echo $team ['team_badge'] ?>" class="card-img-mid" alt="...">
+              <div class="card-body">
+                <p class="card-text text-center"> <?php echo $team ['team_name'] ?></p>
+              </div>
+            </div>  
           </div>
+          <?php }?>
         </div>
       </div>
     </div>
